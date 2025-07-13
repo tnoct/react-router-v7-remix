ec15d3d9dd1942a40afd275cac060b58ac752aae
tnoct <tauqeernasir008@gmail.com>
2025-07-13 18:07:35 +0500

    Change the default route name to index tsx

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("posts", "layouts/posts/layout.tsx", [
-    index("routes/posts/list.tsx"), // Start the Post page, open by default on hitting localhost:port/posts
+    index("routes/posts/index.tsx"), // Start the Post page, open by default on hitting localhost:port/posts
     route(":id", "routes/posts/post.tsx"), // Show one post.
   ]),
 ] satisfies RouteConfig;
