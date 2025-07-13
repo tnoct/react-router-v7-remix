ee8f56b0551932b46dc8806e8c48a1314f19d1cc
tnoct <tauqeernasir008@gmail.com>
2025-07-13 19:05:36 +0500

    Added navigation loader

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigation,
 } from "react-router";
 
 import type { Route } from "./+types/root";
@@ -24,6 +25,8 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <html lang="en">
       <head>
@@ -33,7 +36,34 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        {isNavigating && (
+          <div className="flex absolute min-w-screen bg-transparent backdrop-blur-sm flex-col items-center justify-center min-h-screen">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="mr-1 size-10 text-zinc-900 motion-safe:animate-spin"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span className="text-center mt-4">changing to new url</span>
+          </div>
+        )}
         {children}
+
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,23 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("posts", "layouts/posts/layout.tsx", [
-    index("routes/posts/index.tsx"), // Start the Post page, open by default on hitting localhost:port/posts
-    route(":id", "routes/posts/post.tsx"), // Show one post.
+  // first method of nested routing
+  // route("posts", "layouts/posts/layout.tsx", [
+  //   index("routes/posts/index.tsx"), // Start the Post page, open by default on hitting localhost:port/posts
+  //   route(":id", "routes/posts/post.tsx"), // Show one post.
+  // ]),
+  // 2nd method of nested routing
+  layout("layouts/posts/layout.tsx", [
+    ...prefix("posts", [
+      index("routes/posts/index.tsx"), // Start the Post page, open by default on hitting localhost:port/posts
+      route(":id", "routes/posts/post.tsx"), // Show one post.
+    ]),
   ]),
 ] satisfies RouteConfig;
